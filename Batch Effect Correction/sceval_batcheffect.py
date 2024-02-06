f21eef6e7ddb11e5bb26dd8050c5bdaf7ca1cb12
Ahmed Elnaggar <amit.najjar@gmail.com>
2024-02-06 11:23:21 +0100

    remove masked_ce_loss
    masked_ce_loss doesn't exist

--- Batch Effect Correction/sceval_batcheffect.py
+++ Batch Effect Correction/sceval_batcheffect.py
@@ -26,7 +26,6 @@ from scgpt.loss import (
     masked_mse_loss,
     masked_relative_error,
     criterion_neg_log_bernoulli,
-    masked_ce_loss
 )
 import scgpt as scg
 from scgpt.preprocess import Preprocessor
